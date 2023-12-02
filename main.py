802855f78faa8f6e16947b5a89b6e2048de6dfe9
aidenpike <aidenpike@gmail.com>
2023-12-01 22:26:01 -0500

    Stat lister

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 with open("spells.json", 'r') as file:
     spell_read = json.load(file)
 
-print(spell_read)    
-#spell_search = str(input("Enter spell you would like to look at: "))
+spell_search = str(input("Enter spell you would like to look at: "))
 
-print("Spell Name: " + spell_read['spell']['visual']['name'])
-    
+print("Spell Name:", spell_search)
+print("Spell Color:", spell_read[spell_search]['visual']['color'])
+print("Spell Damage:", spell_read[spell_search]['properties']['damage']['damage_amount'])    
 
 
 
