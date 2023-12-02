1663f19db677729eec94729acb8f4fe0549e2069
aidenpike <aidenpike@gmail.com>
2023-12-01 21:57:00 -0500

    Introduce parsing

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import json
 
 with open("spells.json", 'r') as file:
     spell_read = json.load(file)
+    
+key_input = input("What would  you like to access?: ")
 
-print(spell_read)
+print(spell_read['spell']['visual'][key_input])
