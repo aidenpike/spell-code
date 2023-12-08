db410a364ca0c4679a813021bd041668e4704baa
aidenpike <aidenpike@gmail.com>
2023-12-07 22:45:27 -0500

    Move on click

--- main.py
+++ main.py
@@ -20,28 +20,36 @@ print("Spell Color:", color_rgb)
 print("Spell Damage:", spell_read[spell_search]['properties']['damage']['damage_amount'])    
 
 #Speed calculation: If speed = 1, and framerate is 60, the sprite moves at 1 pixel every 2 frames, so 30 pixels a second
-pos_x = 30
-pos_y = 30
+pos_x = 0
+pos_y = 100
 
 loop = True
+key_pressed = False
+
+if spell_read[spell_search]['properties']['movement']['speed'] < 10:
+    print("Speed too low! Setting to default of 10.")
+    spell_read[spell_search]['properties']['movement']['speed'] = 10
 
 while loop:
+    key_stroke = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = False
+    
+    if key_stroke[pygame.K_d]:
+        key_pressed = True
             
-    key_stroke = pygame.key.get_pressed()
-
-    if key_stroke[pygame.K_d] and pos_x !=  800:
-        print(round(pos_x))
+    if key_pressed and pos_x <= window.get_width() - 20: 
         pos_x += spell_read[spell_search]['properties']['movement']['speed']/frame_clock
 
-    if key_stroke[pygame.K_a] and pos_x > 0:
-        print(round(pos_y))
-        pos_x -= spell_read[spell_search]['properties']['movement']['speed']/frame_clock
-            
+    if pos_x == window.get_width() - 20:
+        color_hex = "000000"
+        color_rgb = tuple(int(color_hex[i:i+2], 16) for i in (0, 2, 4))
+        print("Hit!", spell_read[spell_search]['properties']['damage']['damage_amount'], spell_read[spell_search]['properties']['damage']['damage_type'], "Damage")
+
     window.fill((0, 0, 0))
 
-    pygame.draw.rect(window, (color_rgb), pygame.Rect(pos_x, pos_y, 50, 50))
+    pygame.draw.circle(window, (color_rgb), (pos_x, pos_y), 30)
     
     pygame.display.update()
